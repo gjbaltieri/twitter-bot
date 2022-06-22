80b6d032f38d083eebc1a0b58d5b2d85e4c7b048
Gustavo Jonas Baltieri <104153413+gjbaltieri@users.noreply.github.com>
2022-06-22 17:35:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,10 +25,22 @@ async function getURL() {
     let data = await axios.get(urlAPI)
     console.log(data.data[0].url)
     let url = data.data[0].url;
-    const mediaId = await client.v1.uploadMedia(url);
+    https.get(url, (res) => {
+        const path = `${__dirname}/images/img.jpeg`;
+        const filePath = fs.createWriteStream(path);
+        res.pipe(filePath);
+        filePath.on('finish', () => {
+            filePath.close();
+            console.log('Download Completed');
+        })
+    })
+
+    const mediaId = await client.v1.uploadMedia('./images/img.jpeg');
     const newTweet = await client.v2.tweet({"text": '', "media": {"media_ids": [mediaId]}});
+    console.log(newTweet)
     }
 
 
-setInterval(getURL, 10000)
+// setInterval(getURL, 300000)
+getURL()
 
