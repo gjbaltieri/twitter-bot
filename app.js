fc27b0473ce3bf3d2b9fd0ee3e9847d6b72d96c7
Gustavo Jonas Baltieri <104153413+gjbaltieri@users.noreply.github.com>
2022-06-22 14:53:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const fs = require('fs');
+const https = require('https');
+const axios = require('axios');
+const twApiModule = require('twitter-api-v2')
+const TwitterApi = twApiModule.default;
+
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET_KEY,
+    accessToken: process.env.TWITTER_ACESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACESS_TOKEN_SECRET,
+});
+
+async function getURL() {
+    let urlAPI = process.env.API_URL
+    let data = await axios.get(urlAPI)
+    console.log(data.data[0].url)
+    let url = data.data[0].url;
+    https.get(url, (res) => {
+        const path = `${__dirname}/images/img.jpeg`;
+        const filePath = fs.createWriteStream(path);
+        res.pipe(filePath);
+        filePath.on('finish', () => {
+            filePath.close();
+            console.log('Download Completed');
+        })
+    })
+
+    const mediaId = await client.v1.uploadMedia('./images/img.jpeg');
+    const newTweet = await client.v2.tweet({"text": '', "media": {"media_ids": [mediaId]}});
+    console.log(newTweet)
+}
+
+
+getURL()
